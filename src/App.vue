eb6eeb77fa8a0cdcdceaf49a4c41d0905af1de42
Antonio Molinari <antoniomolinari@me.com>
2026-01-14 13:06:23 +0100

    feat: initialize Vue 3 project with Vite and Tailwind CSS
    - Add package.json with project metadata and dependencies
    - Create main application file (main.js) to bootstrap Vue app
    - Add App.vue with a simple "Hello world!" template
    - Include Tailwind CSS in style.css for styling
    - Set up Vite configuration (vite.config.js) with Vue and Tailwind plugins
    - Add SVG assets for Vite and Vue logos

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+ <main class="h-screen w-screen m-auto container flex items-center justify-center border">
+  <h1 class="text-3xl font-bold text-center p-4">
+    Hello world!
+  </h1>
+ </main>
+ 
+</template>
+
+<style scoped>
+
+</style>
